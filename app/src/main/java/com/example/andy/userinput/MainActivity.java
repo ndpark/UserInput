0dea11c090e99cc0b6d83fc6d2e92e6a15e8bbd9
Andy Park <hg4park@uwaterloo.ca>
2017-05-31 23:21:28 -0400

    Trying to add error message if # < 0

--- app/src/main/java/com/example/andy/userinput/MainActivity.java
+++ app/src/main/java/com/example/andy/userinput/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.andy.userinput;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    private int numberCoffee = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +28,28 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberCoffee = 2;
         display(numberCoffee);
         displayPrice(numberCoffee * 5);
     }
 
     public void increment(View view){
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("3");
+        numberCoffee++;
+        quantityTextView.setText(Integer.toString(numberCoffee));
+        displayPrice(numberCoffee * 5);
     }
 
     public void decrement(View view){
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("1");
+        numberCoffee--;
+        quantityTextView.setText(Integer.toString(numberCoffee));
+        if(getPrice(numberCoffee) < 0 ){
+            toast.show();
+        } else{
+            displayPrice(getPrice(numberCoffee));
+        }
+
+
     }
 
     /**
@@ -47,6 +63,31 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private int getPrice(int numberCoffee){
+        return numberCoffee * 5;
+    }
+
+    public class Warning extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.dialog_fire_missiles)
+                    .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
     }
 
 }
